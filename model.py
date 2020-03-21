3c6e0ae400542b84c6529726a03f6b6dad319071
FZfangzheng <774380232@qq.com>
2020-03-21 14:20:39 +0800

    convert to numpy

--- model.py
+++ model.py
@@ -92,8 +92,8 @@ class FusionNet(nn.Module):
             l21 = lsr_landsat1.mul(torch.div(pre_lsr_landsat2, pre_lsr_landsat1))
             l23 = lsr_landsat3.mul(torch.div(pre_lsr_landsat2, pre_lsr_landsat3))
 
-            p1 = 1 / getads(np.array(torch.sub(pre_lsr_landsat2, pre_lsr_landsat1)))
-            p3 = 1 / getads(np.array(torch.sub(pre_lsr_landsat2, pre_lsr_landsat3)))
+            p1 = 1 / getads((torch.sub(pre_lsr_landsat2, pre_lsr_landsat1)).cpu().numpy())
+            p3 = 1 / getads((torch.sub(pre_lsr_landsat2, pre_lsr_landsat3)).cpu().numpy())
             w1 = p1 / (p1 + p3)
             w3 = p3 / (p1 + p3)
 
@@ -105,8 +105,8 @@ class FusionNet(nn.Module):
             l21 = inputs[1].mul(torch.div(pre_landsat2, pre_landsat1))
             l23 = inputs[3].mul(torch.div(pre_landsat2, pre_landsat3))
 
-            p1 = 1 / getads(np.array(torch.sub(pre_landsat2, pre_landsat1)))
-            p3 = 1 / getads(np.array(torch.sub(pre_landsat2, pre_landsat3)))
+            p1 = 1 / getads((torch.sub(pre_landsat2, pre_landsat1)).cpu().numpy())
+            p3 = 1 / getads((torch.sub(pre_landsat2, pre_landsat3)).cpu().numpy())
             w1 = p1 / (p1 + p3)
             w3 = p3 / (p1 + p3)
 
